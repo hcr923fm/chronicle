8d9c970593909ad2d9ddf6678d4140df5a50ef4c
Cal McLean <calmcl@aol.com>
2017-01-13 23:38:38 +0000

    It works! Basic on-the-hour every-hour timer

--- chronicle.h
+++ chronicle.h
@@ -1,8 +1,18 @@
 #include "RtAudio.h"
+extern "C"{
+	#include "sndfile.h"
+}
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <thread>
+#include <iomanip>
 
 using namespace std;
 
 // So, you've decided to make some logger software...
 
-int main();
+int main(int argc, char* argv[]);
+void doRecord(char* directory);
+int cb_record(void *outputBuffer, void *inputBuffer, unsigned int nFrames, double streamTime, RtAudioStreamStatus status, void *userData);
