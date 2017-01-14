e38668ca2a415d593ea97caf15daf0a4ea424a62
Cal McLean <calmcl@aol.com>
2017-01-14 17:02:22 +0000

    Added signal handling for graceful shutdown, fixes #1
    Former-commit-id: b01264c4d42126a7b65200433ceec65595c5e48b

--- chronicle.h
+++ chronicle.h
@@ -8,6 +8,12 @@ extern "C"{
 #include <chrono>
 #include <thread>
 #include <iomanip>
+#include <csignal>
+
+
+std::string const SOFTWARE_NAME = "chronicle";
+std::string const VERSION = "0.1.0";
+std::string const AUTHOR = "Callum McLean";
 
 using namespace std;
 
@@ -16,3 +22,5 @@ using namespace std;
 int main(int argc, char* argv[]);
 void doRecord(char* directory);
 int cb_record(void *outputBuffer, void *inputBuffer, unsigned int nFrames, double streamTime, RtAudioStreamStatus status, void *userData);
+void stopRecord();
+void signalHandler(int sigNum);
