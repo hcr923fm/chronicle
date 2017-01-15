ae1b4de7646142aa0fc4c6c451d4173b6c3b8502
Cal McLean <calmcl@aol.com>
2017-01-15 19:57:08 +0000

    Added command-line switches; licence details

--- chronicle.h
+++ chronicle.h
@@ -12,15 +12,17 @@ extern "C"{
 
 
 std::string const SOFTWARE_NAME = "chronicle";
-std::string const VERSION = "0.1.0";
-std::string const AUTHOR = "Callum McLean";
+std::string const VERSION = "0.1.1";
 
 using namespace std;
 
 // So, you've decided to make some logger software...
 
 int main(int argc, char* argv[]);
-void doRecord(char* directory);
+void doRecord(const char* directory);
 int cb_record(void *outputBuffer, void *inputBuffer, unsigned int nFrames, double streamTime, RtAudioStreamStatus status, void *userData);
 void stopRecord();
 void signalHandler(int sigNum);
+
+void printLicence();
+void printHelp();
