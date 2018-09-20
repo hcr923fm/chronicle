c49865c1e019595e3c2725ffb91acc3de4066a5b
calmcl1 <calmcl1@aol.com>
2018-09-20 14:06:57 +0100

    Correct dist-script to use correct file paths, update gitignore for generated files

--- dist_script.sh
+++ dist_script.sh
@@ -23,7 +23,7 @@ cp ../CHANGELOG.md .
 cd ..
 tar zcf chronicle-v1.0.0-x32.tar.gz chronicle-v1.0.0-x32
 
-cd /chronicle-v1.0.0-x64
+cd chronicle-v1.0.0-x64
 ../configure --disable-shared --enable-static --host=x86_64-w64-mingw32 && make && make mostlyclean
 deps=$(ldd ./chronicle.exe | awk '/~*mingw*/ {print $3}')
 for dep in $deps
