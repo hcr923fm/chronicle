0d0cb33b257abdaf92b36cb44c69a1494bb451e1
calmcl1 <calmcl1@aol.com>
2018-09-29 15:35:18 +0100

    install python on windows build to satisfy libsndfile build

--- .ci/mingw-64/apt-script.sh
+++ .ci/mingw-64/apt-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update -qq
-apt-get install mingw-w64 mingw-w64-tools pkg-config git libtool libtool-bin build-essential wget autoconf automake -qq -y
+apt-get install mingw-w64 mingw-w64-tools pkg-config git libtool libtool-bin build-essential wget autoconf automake python -qq -y
 update-alternatives --set x86_64-w64-mingw32-gcc /usr/bin/x86_64-w64-mingw32-gcc-posix
 update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++-posix
