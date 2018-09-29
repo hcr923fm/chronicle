0d37af4db109b47e83b514f96e394b5787154e99
calmcl1 <calmcl1@aol.com>
2018-09-29 15:48:30 +0100

    set windows script permission

--- .ci/mingw-64/apt-script.sh
+++ .ci/mingw-64/apt-script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chmod +x .ci/mingw-64//*.sh
 apt-get update -qq
 apt-get install mingw-w64 mingw-w64-tools pkg-config git libtool libtool-bin build-essential wget autoconf automake python -qq -y
 update-alternatives --set x86_64-w64-mingw32-gcc /usr/bin/x86_64-w64-mingw32-gcc-posix
