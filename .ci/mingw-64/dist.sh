6be49fc70466b6f990a72dad1e075f28a904c1b1
calmcl1 <calmcl1@aol.com>
2018-09-29 16:24:42 +0100

    Copy windows deps to local dir

--- .ci/mingw-64/dist.sh
+++ .ci/mingw-64/dist.sh
@@ -1,5 +1,12 @@
 cd ${CHRONICLE_TRIPLET}
 make mostlyclean
+deps=`ldd chronicle.exe | awk '$0 1~ /mingw/ {print $3;}'`
+echo `ldd chronicle.exe`
+for dep in ${deps}
+do
+    echo "Copying ${dep} to $PWD"
+    cp ${dep} .
+done
 cp ../LICENCE .
 cp ../README.md .
 cp ../CHANGELOG.md .
