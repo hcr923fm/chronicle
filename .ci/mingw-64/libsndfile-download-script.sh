f3df045244cefed070c21464ea6649f6bc1e01b2
calmcl1 <calmcl1@aol.com>
2018-09-29 15:35:18 +0100

    install python on windows build to satisfy libsndfile build

--- .ci/mingw-64/libsndfile-download-script.sh
+++ .ci/mingw-64/libsndfile-download-script.sh
@@ -2,4 +2,4 @@
 wget http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28.tar.gz
 tar xf libsndfile-1.0.28.tar.gz
 cd libsndfile-1.0.28
-./configure --host=x86_64-w64-mingw32 --build=x86_64-unknown-linux-gnu
+./configure --host=x86_64-w64-mingw32
