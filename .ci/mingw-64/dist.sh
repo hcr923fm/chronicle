eb1036a61b93ebea9e41e32caf2d97313c0e4bc7
calmcl1 <calmcl1@aol.com>
2018-09-29 21:53:07 +0200

    things work best when you get the `find` syntax right

--- .ci/mingw-64/dist.sh
+++ .ci/mingw-64/dist.sh
@@ -1,11 +1,9 @@
 cd ${CHRONICLE_TRIPLET}
 make mostlyclean
 deps=`x86_64-w64-mingw32-objdump -p chronicle.exe | grep .dll | awk '{print $3}'`
-echo "Deps:"
-echo ${deps}
 for dep in ${deps}
 do
-    dep_file=`find /usr name ${dep} | grep .dll -m 1`
+    dep_file=`find /usr -name ${dep} | grep .dll -m 1`
     echo "Copying ${dep_file} to $PWD"
     cp ${dep_file} .
 done
