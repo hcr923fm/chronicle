dde7cadc8561d82a391a7b7830a9746a36571b82
calmcl1 <calmcl1@aol.com>
2018-09-29 14:36:04 +0100

    Try using update-alternatives to use posix version of g++ on windows

--- .ci/mingw-64/apt-script.sh
+++ .ci/mingw-64/apt-script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 apt-get update -qq
 apt-get install mingw-w64 mingw-w64-tools pkg-config git libtool libtool-bin build-essential wget autoconf automake -qq -y
+sudo update-alternatives --set x86_64-w64-mingw32-gcc /usr/bin/x86_64-w64-mingw32-gcc-posix
+sudo update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++-posix
