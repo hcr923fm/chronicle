56c7696208d6b8f7c76e2803955b6cbf0eef59f7
calmcl1 <calmcl1@aol.com>
2018-09-29 23:13:25 +0200

    Try more stuff

--- .ci/mingw-64/chronicle-build.sh
+++ .ci/mingw-64/chronicle-build.sh
@@ -3,4 +3,4 @@ chmod +x autogen.sh
 ./autogen.sh
 mkdir -p ${CHRONICLE_TRIPLET}
 cd ${CHRONICLE_TRIPLET}
-../configure --disable-shared --enable-static --host=x86_64-w64-mingw32 --prefix=/usr/local && make
+../configure --host=x86_64-w64-mingw32 && make
