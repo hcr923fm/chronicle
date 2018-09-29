e8dd9ef5fee337e3e2226fb0ba5eba44c9d2a987
calmcl1 <calmcl1@aol.com>
2018-09-29 22:12:20 +0200

    maybe only grep dll?

--- .ci/mingw-64/dist.sh
+++ .ci/mingw-64/dist.sh
@@ -1,9 +1,9 @@
 cd ${CHRONICLE_TRIPLET}
 make mostlyclean
-deps=`x86_64-w64-mingw32-objdump -p chronicle.exe | grep .dll | awk '{print $3}'`
+deps=`x86_64-w64-mingw32-objdump -p chronicle.exe | grep dll | awk '{print $3}'`
 for dep in ${deps}
 do
-    dep_file=`find /usr -name ${dep} | grep .dll -m 1`
+    dep_file=`find /usr -name ${dep} | grep dll -m 1`
     echo "Copying ${dep_file} to $PWD"
     cp ${dep_file} .
 done
