5ee969c4a68913c9d885ae878a04d90b321f4ad1
calmcl1 <calmcl1@aol.com>
2018-09-29 10:42:10 +0100

    Add linux dist script

---
+++ .ci/linux/dist-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd chronicle-v1.0.0-linux
+make mostlyclean
+cp ../LICENCE .
+cp ../README.md .
+cp ../CHANGELOG.md .
+cd ..
+tar zcf chronicle-v1.0.0-linux.tar.gz chronicle-v1.0.0-linux
