3b7c9be8b12b99cc8052dae473fd5d626f2f6d7b
calmcl1 <calmcl1@aol.com>
2018-09-29 18:44:29 +0100

    Update dep-finding process, though crude

--- .ci/mingw-64/dist.sh
+++ .ci/mingw-64/dist.sh
@@ -1,11 +1,11 @@
 cd ${CHRONICLE_TRIPLET}
 make mostlyclean
-deps=`ldd chronicle.exe | awk '$0 1~ /mingw/ {print $3;}'`
-echo `ldd chronicle.exe`
+deps=`x86_64-w64-mingw32-objdump -p chronicle.exe | grep dll | awk '{print $3}'`
 for dep in ${deps}
 do
-    echo "Copying ${dep} to $PWD"
-    cp ${dep} .
+    dep_file = `find /usr name ${dep} | grep dll -m 1`
+    echo "Copying ${dep_file} to $PWD"
+    cp ${dep_file} .
 done
 cp ../LICENCE .
 cp ../README.md .
