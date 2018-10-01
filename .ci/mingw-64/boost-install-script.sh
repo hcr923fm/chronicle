541f4af5878d138ade6697653fec8ee17180bd3c
calmcl1 <calmcl1@aol.com>
2018-10-01 14:45:45 +0100

    try different boost opts
    Former-commit-id: 4cc481f5f6d5d6c6e4a6d12f28c1265c4af4cdb8

--- .ci/mingw-64/boost-install-script.sh
+++ .ci/mingw-64/boost-install-script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd boost_1_68_0
-./b2 --user-config=user-config.jam toolset=gcc target-os=windows release address-model=64 architecture=x86 install
+./b2 --user-config=user-config.jam --build-type=complete toolset=gcc target-os=windows link=static release address-model=64 architecture=x86 install
