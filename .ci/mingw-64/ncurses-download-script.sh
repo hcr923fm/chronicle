8ef55ad691292d6a87c12af23a26f1eec20a7d43
calmcl1 <calmcl1@aol.com>
2018-10-01 13:05:54 +0100

    Added explicit mingw prefixes to windows build, as --host wasn't always respected; added libflac, libogg, libvorbis
    Former-commit-id: 295c391028e9bbcce1b095934d2cea23fecbbac6

--- .ci/mingw-64/ncurses-download-script.sh
+++ .ci/mingw-64/ncurses-download-script.sh
@@ -2,4 +2,4 @@
 wget ftp://ftp.invisible-island.net/ncurses/ncurses-6.1.tar.gz
 tar xf ncurses-6.1.tar.gz
 cd ncurses-6.1
-./configure --enable-term-driver --enable-sp-funcs --host=x86_64-w64-mingw32 --with-build-cc=gcc --with-pkg-config-libdir=${PKG_CONFIG_PATH} --enable-pc-files --prefix=/usr/local
+./configure --enable-term-driver --enable-sp-funcs --host=x86_64-w64-mingw32 --with-build-cc=gcc --with-pkg-config-libdir=${PKG_CONFIG_PATH} --enable-pc-files --prefix=/usr/x86_64-w64-mingw32
