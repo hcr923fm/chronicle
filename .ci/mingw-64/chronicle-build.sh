55380a884c9934b37f70b06e14f1f8855afff839
calmcl1 <calmcl1@aol.com>
2018-10-05 18:59:39 +0100

    Used local prefix, corrected line endings
    Former-commit-id: e58ea437d6743c015a89ab616b4d57923107fc2e

--- .ci/mingw-64/chronicle-build.sh
+++ .ci/mingw-64/chronicle-build.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
 chmod +x autogen.sh
 ./autogen.sh
 mkdir -p ${CHRONICLE_TRIPLET}
 cd ${CHRONICLE_TRIPLET}
-../configure --host=x86_64-w64-mingw32 --disable-shared --enable-static --prefix=/usr/x86_64-w64-mingw32
+../configure --host=x86_64-w64-mingw32 --disable-shared --enable-static --prefix=/usr/local --with-boost=/usr/local/include/boost-1_68 --with-boost-libdir=/usr/local/lib #--prefix=/usr/x86_64-w64-mingw32
 LDFLAGS=--trace
+CPPFLAGS=-D_ISOC99_SOURCE
 make
