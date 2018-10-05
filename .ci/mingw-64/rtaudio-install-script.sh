a6d4a94d385379364a814a2356dc8368397c3097
calmcl1 <calmcl1@aol.com>
2018-10-05 19:41:04 +0100

    Re-add caches to cirrus.yml, update path
    Former-commit-id: 0afa6bebd7d7ed48933dc843f2daf70e39ef419e

--- .ci/mingw-64/rtaudio-install-script.sh
+++ .ci/mingw-64/rtaudio-install-script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd rtaudio
+./autogen.sh --no-configure
+export CFLAGS=-Wno-unused-but-set-variable # We need this for the RtAudio build
+./configure --host=x86_64-w64-mingw32 --disable-shared --prefix=/usr/local #--prefix=/usr/x86_64-w64-mingw32
 make
 make install
