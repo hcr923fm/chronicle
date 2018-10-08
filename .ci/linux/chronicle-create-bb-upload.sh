6d6a846b34e9b80535629de22d154a88b495c627
calmcl1 <calmcl1@aol.com>
2018-10-08 14:48:18 +0100

    getting the params right helps

--- .ci/linux/chronicle-create-bb-upload.sh
+++ .ci/linux/chronicle-create-bb-upload.sh
@@ -1,4 +1,4 @@
-b2 authorize_application $BACKBLAZE_APP_KEY_ID $BACKBLAZE_APP_KEY
+b2 authorize-account $BACKBLAZE_APP_KEY_ID $BACKBLAZE_APP_KEY
 b2 upload_file chronicle-releases ${CHRONICLE_TRIPLET}.tar.gz ${CHRONICLE_VERSION}/${CHRONICLE_TRIPLET}.tar.gz
 cd ${CHRONICLE_TRIPLET}
 b2 upload_file chronicle-releases ${CHRONICLE_NAME}-${CHRONICLE_VERSION}.tar.gz ${CHRONICLE_VERSION}/${CHRONICLE_NAME}-${CHRONICLE_VERSION}-src.tar.gz
--- .ci/mingw-64/chronicle-create-bb-upload.sh
+++ .ci/mingw-64/chronicle-create-bb-upload.sh
@@ -1,3 +1,3 @@
-b2 authorize_application $BACKBLAZE_APP_KEY_ID $BACKBLAZE_APP_KEY
+b2 authorize-account $BACKBLAZE_APP_KEY_ID $BACKBLAZE_APP_KEY
 b2 upload_file chronicle-releases ${CHRONICLE_TRIPLET}.tar.gz ${CHRONICLE_VERSION}/${CHRONICLE_TRIPLET}.tar.gz
 b2 upload_file chronicle-releases ${CHRONICLE_TRIPLET}.zip ${CHRONICLE_VERSION}/${CHRONICLE_TRIPLET}.zip
