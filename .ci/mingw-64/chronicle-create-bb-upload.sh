93a2f59be5ad83acd0c080d47ad14d5e819eac50
calmcl1 <calmcl1@aol.com>
2018-10-08 14:43:59 +0100

    Form b2 command correctly

--- .ci/mingw-64/chronicle-create-bb-upload.sh
+++ .ci/mingw-64/chronicle-create-bb-upload.sh
@@ -1,3 +1,3 @@
 b2 authorize_application $BACKBLAZE_APP_KEY_ID $BACKBLAZE_APP_KEY
-b2 upload_file ${CHRONICLE_TRIPLET}.tar.gz chronicle-releases ${CHRONICLE_VERSION}/${CHRONICLE_TRIPLET}.tar.gz
-b2 upload_file ${CHRONICLE_TRIPLET}.zip chronicle-releases ${CHRONICLE_VERSION}/${CHRONICLE_TRIPLET}.zip
+b2 upload_file chronicle-releases ${CHRONICLE_TRIPLET}.tar.gz ${CHRONICLE_VERSION}/${CHRONICLE_TRIPLET}.tar.gz
+b2 upload_file chronicle-releases ${CHRONICLE_TRIPLET}.zip ${CHRONICLE_VERSION}/${CHRONICLE_TRIPLET}.zip
