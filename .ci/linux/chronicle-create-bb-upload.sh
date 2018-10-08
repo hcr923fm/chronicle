1d4a9dcefff462f15f8db1f2090f7d9c7d42eb10
calmcl1 <calmcl1@aol.com>
2018-10-08 14:39:02 +0100

    Moved pip install into separate command, otherwise shell doesn't update

--- .ci/linux/chronicle-create-bb-upload.sh
+++ .ci/linux/chronicle-create-bb-upload.sh
@@ -1,4 +1,3 @@
-pip install --upgrade b2
 b2 authorize_application $BACKBLAZE_APP_KEY_ID $BACKBLAZE_APP_KEY
 b2 upload_file ${CHRONICLE_TRIPLET}.tar.gz chronicle-releases ${CHRONICLE_VERSION}/${CHRONICLE_TRIPLET}.tar.gz
 cd ${CHRONICLE_TRIPLET}
