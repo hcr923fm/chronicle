a59607bf29d0fc0b50881c68903bf388f60f811c
calmcl1 <calmcl1@aol.com>
2019-08-21 17:57:44 +0100

    Add new CMake toolchains for x-compiling

--- .ci/mingw32-32/chronicle-build.sh
+++ .ci/mingw32-32/chronicle-build.sh
@@ -5,4 +5,4 @@ cd ${CHRONICLE_TRIPLET}
 # ../configure --host=i686-w64-mingw32 --with-boost=/usr/i686-w64-mingw32/include/boost-1_68 --with-boost-libdir=/usr/i686-w64-mingw32/lib --prefix=/usr/i686-w64-mingw32
 CPPFLAGS="-D_ISOC99_SOURCE -Wl,--trace"
 cmake ..
-cmake --build .
+cmake -DCMAKE_TOOLCHAIN_FILE=../.ci/mingw32-32/win32_toolchain.cmake ..
--- .ci/mingw32-64/chronicle-build.sh
+++ .ci/mingw32-64/chronicle-build.sh
@@ -4,5 +4,5 @@ mkdir -p ${CHRONICLE_TRIPLET}
 cd ${CHRONICLE_TRIPLET}
 # ../configure --host=x86_64-w64-mingw32 --with-boost=/usr/x86_64-w64-mingw32/include/boost-1_68 --with-boost-libdir=/usr/x86_64-w64-mingw32/lib --prefix=/usr/x86_64-w64-mingw32
 CPPFLAGS="-D_ISOC99_SOURCE -Wl,--trace"
-cmake ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../.ci/mingw32-64/win64_toolchain.cmake ..
 cmake --build .
