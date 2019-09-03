99101241bc48280730da568253ff76c2812cbe8c
calmcl1 <calmcl1@aol.com>
2019-09-03 11:21:53 +0100

    Try and force the CXX compiler for RTAudio

--- .ci/mingw32-64/rtaudio-install-script.sh
+++ .ci/mingw32-64/rtaudio-install-script.sh
@@ -2,6 +2,6 @@
 cd rtaudio
 ./autogen.sh --no-configure
 export CFLAGS=-Wno-unused-but-set-variable # We need this for the RtAudio build
-./configure --host=x86_64-w64-mingw32 --disable-shared --prefix=/usr/x86_64-w64-mingw32
+./configure CXX=/usr/bin/x86_64-w64-mingw32-g++-posix --host=x86_64-w64-mingw32 --disable-shared --prefix=/usr/x86_64-w64-mingw32
 make
 make install
