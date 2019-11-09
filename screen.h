1513c17d6cbbc763ba2083d6c57c1e320728ff9f
Cal McLean <calmcl1@aol.com>
2019-11-09 09:31:54 +0100

    declare curses as C

--- screen.h
+++ screen.h
@@ -2,7 +2,9 @@
 #define __SCREEN_H__
 
 #ifdef _WIN32
-#include <curses.h>
+extern "C"
+{
+#include <curses.h>}
 #else
 #include <ncurses.h>
 #endif
@@ -11,20 +13,20 @@
 #include <cstring>
 #include <algorithm>
 
-using namespace std;
+    using namespace std;
 
-void initCurses(string windowTitle);
-void closeCurses();
-void calculateWindowPositions();
+    void initCurses(string windowTitle);
+    void closeCurses();
+    void calculateWindowPositions();
 
-void setWindowTitle(string windowTitle);
-void setBorderAndDividers();
-void updateRecordingToPath(string filePath);
-void updateAudioDevice(string audioDevice, int sampleRate, int channelCount);
-void updateAudioMeter(int channelNum, float maxVal, float currentVal, string volumeLabel);
-void updateHardDriveSpace(long spaceAvailBeforeGB, long fileSizeMB);
-void onWindowResize();
+    void setWindowTitle(string windowTitle);
+    void setBorderAndDividers();
+    void updateRecordingToPath(string filePath);
+    void updateAudioDevice(string audioDevice, int sampleRate, int channelCount);
+    void updateAudioMeter(int channelNum, float maxVal, float currentVal, string volumeLabel);
+    void updateHardDriveSpace(long spaceAvailBeforeGB, long fileSizeMB);
+    void onWindowResize();
 
-extern bool NC_UI_IS_ENABLED;
+    extern bool NC_UI_IS_ENABLED;
 
 #endif //__SCREEN_H__
